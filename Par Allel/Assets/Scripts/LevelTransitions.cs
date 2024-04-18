9dd61512ca13ba108b85227eb5c11f4ce69d85c9
GavinTafoya <83233027+GavinTafoya@users.noreply.github.com>
2024-04-17 21:10:01 -0600

    bingus

--- Par Allel/Assets/Scripts/LevelTransitions.cs
+++ Par Allel/Assets/Scripts/LevelTransitions.cs
@@ -18,14 +18,14 @@ public class LevelTransitions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
-        camera2 = GameObject.Find("Second Camera");
-        DontDestroyOnLoad(camera2);
-        virtualCam1 = GameObject.Find("Virtual Camera");
-        DontDestroyOnLoad(virtualCam1);
-        virtualCam2 = GameObject.Find("Virtual Camera (1)");
-        DontDestroyOnLoad(virtualCam2);
-        SceneManager.activeSceneChanged += FindCameraTarget;
+        //DontDestroyOnLoad(gameObject);
+        //camera2 = GameObject.Find("Second Camera");
+        //DontDestroyOnLoad(camera2);
+        //virtualCam1 = GameObject.Find("Virtual Camera");
+        //DontDestroyOnLoad(virtualCam1);
+        //virtualCam2 = GameObject.Find("Virtual Camera (1)");
+        //DontDestroyOnLoad(virtualCam2);
+        //SceneManager.activeSceneChanged += FindCameraTarget;
     }
 
     public void NextLevel()
