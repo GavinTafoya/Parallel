bea334cddb8858d0849905e0476f9686e5c19297
Joaquin <joaquinrodriguez@u.boisestate.edu>
2024-04-18 14:40:58 -0600

    BUTTON CONTROLS YIPPEE

--- Par Allel/Assets/Scripts/TouchControls.cs
+++ Par Allel/Assets/Scripts/TouchControls.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.UI;
 using ETouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class TouchControls : MonoBehaviour
 {
     // MOBILE INPUT
     [SerializeField] private FloatingJoystick joystick;
+    [SerializeField] private Rect leftButton;
+    [SerializeField] private Rect rightButton;
+    [SerializeField] private Rect jumpButton;
     [SerializeField] private Rect joystickLocation;
     [SerializeField] private InputManager inputManager;
     private Finger movementFinger;
+    private Finger jumpingFinger;
     private Vector2 movementAmount;
+    private bool jumping;
+    // rect transfrom is so broken
+    //private Rect offset = new Rect(50, 50, 0, 0);
 
     private void OnEnable()
     {
@@ -17,12 +25,14 @@ public class TouchControls : MonoBehaviour
         ETouch.Touch.onFingerDown += TouchOnFingerDown;
         ETouch.Touch.onFingerUp += TouchOnFingerUp;
         ETouch.Touch.onFingerMove += TouchOnFingerMove;
+        jumpButton.x = Screen.width - 275;
     }
 
     // so dumb that I need this
+    // 
     public void FindJoystick()
     {
-        joystick = GameObject.Find("Joystick").GetComponent<FloatingJoystick>();
+        Debug.Log("found buttons");
     }
 
     private void OnDisable()
@@ -33,9 +43,10 @@ public class TouchControls : MonoBehaviour
         EnhancedTouchSupport.Enable();
     }
 
+    // empty function woop woop
     private void TouchOnFingerMove(Finger finger)
     {
-        if (finger == movementFinger)
+        /*if (finger == movementFinger)
         {
             Vector2 knobPosition;
             float maxMovement = joystickLocation.width / 2;
@@ -52,7 +63,7 @@ public class TouchControls : MonoBehaviour
 
             joystick.knob.anchoredPosition = knobPosition;
             movementAmount = knobPosition / maxMovement;
-        }
+        }*/
     }
 
     private void TouchOnFingerUp(Finger finger)
@@ -60,8 +71,13 @@ public class TouchControls : MonoBehaviour
         if (finger == movementFinger)
         {
             movementFinger = null;
-            joystick.knob.anchoredPosition = Vector2.zero;
+            //joystick.knob.anchoredPosition = Vector2.zero;
             movementAmount = Vector2.zero;
+        }
+        else if (finger == jumpingFinger)
+        {
+            jumpingFinger = null;
+            jumping = false;
         }
     }
 
@@ -69,11 +85,36 @@ public class TouchControls : MonoBehaviour
     {
         FindJoystick();
 
-        if (movementFinger == null && joystickLocation.Contains(finger.screenPosition))
+        /*if (movementFinger == null && joystickLocation.Contains(finger.screenPosition))
         {
             movementFinger = finger;
             movementAmount = Vector2.zero;
-        } 
+        } */
+        //Debug.LogWarning(leftButton.rect + " " + finger.screenPosition + " " + leftButton.rect.Contains(finger.screenPosition));
+        // bs to fix unity bs
+        /*Rect leftArea = new Rect(offset.x + leftButton.rect.x, offset.y + leftButton.rect.y, leftButton.rect.width, leftButton.rect.height);
+        Rect rightArea = new Rect(offset.x + rightButton.rect.x, offset.y + rightButton.rect.y, rightButton.rect.width, rightButton.rect.height);
+        Rect jumpArea = new Rect(offset.x + jumpButton.rect.x, offset.y + jumpButton.rect.y, jumpButton.rect.width, jumpButton.rect.height);*/
+
+        Debug.LogWarning(jumpButton + " " + finger.screenPosition);
+        if (movementFinger == null && leftButton.Contains(finger.screenPosition))
+        {
+            Debug.LogWarning("lefted");
+            movementFinger = finger;
+            movementAmount = -Vector2.one;
+        }
+        else if (movementFinger == null && rightButton.Contains(finger.screenPosition))
+        {
+            Debug.LogWarning("righted");
+            movementFinger = finger;
+            movementAmount = Vector2.one;
+        }
+        else if (jumpingFinger == null && jumpButton.Contains(finger.screenPosition))
+        {
+            Debug.LogWarning("jumped");
+            jumpingFinger = finger;
+            jumping = true;
+        }
     }
 
     private void Update()
@@ -82,15 +123,16 @@ public class TouchControls : MonoBehaviour
         if (SystemInfo.deviceType == DeviceType.Handheld)
         {
             inputManager.movementInput = movementAmount.x;
-            inputManager.isJumping = movementAmount.y > 0.75f;
+            inputManager.isJumping = jumping;
         }
     }
 
     public void ResetMovement()
     {
         movementFinger = null;
-        joystick.knob.anchoredPosition = Vector2.zero;
+        //joystick.knob.anchoredPosition = Vector2.zero;
         movementAmount = Vector2.zero;
+        jumping = false;
         inputManager.movementInput = 0;
         inputManager.isJumping = false;
     }
