5b29ae957004d7bf91ea65a19d24c516d9861ea3
Joaquin <joaquinrodriguez@u.boisestate.edu>
2024-04-19 15:13:23 -0600

    bazinge

--- Par Allel/Assets/Scripts/MovingPlatform.cs
+++ Par Allel/Assets/Scripts/MovingPlatform.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
+public class MovingPlatform : ActionObject
 {
-    float x = 0;
+    [SerializeField] private float distance;
+    [SerializeField] private float midpoint;
+    private bool moving = false;
+    private float y;
+
+    public override void Action()
+    {
+        moving = true;
+    }
 
     void Update()
     {
-        transform.position = new Vector2(5 / 4 * (3 * Mathf.Sin(x) - Mathf.Sin(3 * x)), 5 / 4 * (Mathf.Cos(x) - Mathf.Cos(3 * x)));
-        x += 2 / Mathf.PI * Time.deltaTime * 4;
+        if (moving)
+        {
+            transform.position = new Vector2(transform.position.x, Mathf.Sin(y * distance / 2) + midpoint);
+            y += Time.deltaTime;
+        }
+        /*transform.position = new Vector2(5 / 4 * (3 * Mathf.Sin(x) - Mathf.Sin(3 * x)), 5 / 4 * (Mathf.Cos(x) - Mathf.Cos(3 * x)));
+        x += 2 / Mathf.PI * Time.deltaTime * 4;*/
     }
 }
