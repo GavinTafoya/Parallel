79ed5bae4519069e7e78789847b0bbc9501e0e4d
Joaquin <joaquinrodriguez@u.boisestate.edu>
2024-04-22 22:14:34 -0600

    mostly level 5

--- Par Allel/Assets/Scripts/MovingPlatform.cs
+++ Par Allel/Assets/Scripts/MovingPlatform.cs
@@ -4,8 +4,9 @@ public class MovingPlatform : ActionObject
 {
     [SerializeField] private float distance;
     [SerializeField] private float midpoint;
+    [SerializeField] private bool isVertical;
     private bool moving = false;
-    private float y;
+    private float delta;
 
     public override void Action()
     {
@@ -16,8 +17,11 @@ public class MovingPlatform : ActionObject
     {
         if (moving)
         {
-            transform.position = new Vector2(transform.position.x, Mathf.Sin(y * distance / 2) + midpoint);
-            y += Time.deltaTime;
+            transform.position = new Vector2(
+                (!isVertical) ? Mathf.Sin(delta * distance / 2) + midpoint : transform.position.x, 
+                (isVertical) ? Mathf.Sin(delta * distance / 2) + midpoint : transform.position.y
+            );
+            delta += Time.deltaTime;
         }
         /*transform.position = new Vector2(5 / 4 * (3 * Mathf.Sin(x) - Mathf.Sin(3 * x)), 5 / 4 * (Mathf.Cos(x) - Mathf.Cos(3 * x)));
         x += 2 / Mathf.PI * Time.deltaTime * 4;*/
